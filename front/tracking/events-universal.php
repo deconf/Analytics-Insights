0bbd7414ed7fd81846118b9417b9fb4a361cb4ac
Alin Marcu <admin@deconf.com>
2014-10-06 14:59:29 +0300

    better detection for outbound links
    better detection for outbound links

--- front/tracking/events-universal.php
+++ front/tracking/events-universal.php
@@ -18,7 +18,7 @@
             //Track Affiliates
             $('a').filter(function() {
             	if ('<?php echo esc_js($GADASH_Config->options['ga_event_affiliates']);?>'!=''){
-                	return this.href.match(/(<?php echo str_replace('/','\/',(esc_js($GADASH_Config->options['ga_event_affiliates'])));?>)/);
+        	       	return this.href.match(/(<?php echo str_replace('/','\/',(esc_js($GADASH_Config->options['ga_event_affiliates'])));?>)/);
             	}	
             }).click(function(event) {
                		ga('send','event', 'affiliates', 'click', this.href<?php if(isset($GADASH_Config->options['ga_event_bouncerate']) && $GADASH_Config->options['ga_event_bouncerate']){echo ", {'nonInteraction': 1}";}?>);
@@ -26,19 +26,13 @@
             <?php } ?>
             
             //Track Outbound Links
-            var loc = location.host.split('.');
-            while (loc.length > 2) { loc.shift(); }
-            loc = loc.join('.');
-            var localURLs = [
-                              loc,
-                              '<?php echo esc_html(get_option('siteurl'));?>'
-                            ];
+            var localURLs = ['<?php $domaindata = $tools->get_root_domain(esc_html(get_option('siteurl'))); echo $domaindata['domain'];?>'];
             $('a[href^="http"]').filter(function() {
-				if (!this.href.match(/.*\.(<?php echo esc_js($GADASH_Config->options['ga_event_downloads']);?>)(\?.*)?$/)){
-	                for (var i = 0; i < localURLs.length; i++) {
-	                    if (this.href.indexOf(localURLs[i]) == -1) return this.href;
-	                }
-				}    
+		if (!this.href.match(/.*\.(<?php echo esc_js($GADASH_Config->options['ga_event_downloads']);?>)(\?.*)?$/)){
+	        	for (var i = 0; i < localURLs.length; i++) {
+	               		if (this.href.indexOf(localURLs[i]) == -1) return this.href;
+	      		}
+		}    
             }).click(function(e) {
                 ga('send','event', 'outbound', 'click', this.href<?php if(isset($GADASH_Config->options['ga_event_bouncerate']) && $GADASH_Config->options['ga_event_bouncerate']){echo ", {'nonInteraction': 1}";}?>);
             });
