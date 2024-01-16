6e19c2b9ede91e347e52bc75c2293ed454bf9007
deconf <nylas@DESKTOP-EUDCGRP>
2024-01-16 20:50:53 +0200

    file cleanup

--- tools/oauth2callback.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-if ($_GET['state'] && $_GET['code']) {
-	$redirect_uri = $_GET['state'] . '&aiwp_access_code=' . $_GET['code'];
-	header('Location: ' . filter_var($redirect_uri, FILTER_SANITIZE_URL));
-}
